1b443bbc71ecb6546d6b54a45f9e22acbf590de0
seolbotdae <seolbotdae@gmail.com>
2024-06-17 19:38:46 +0900

    Initial Commit

---
+++ CarWashDay/CarWashDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarWashDayApp.swift
+//  CarWashDay
+//
+//  Created by Seol WooHyeok on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct CarWashDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
