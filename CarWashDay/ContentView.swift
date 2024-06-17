b9706b477b831219964b39a11e5e1b972e04ae1f
다은0 <jenny8612@naver.com>
2024-06-18 00:43:26 +0900

    메인뷰 상단 텍스트 3줄 추가

--- CarWashDay/ContentView.swift
+++ CarWashDay/ContentView.swift
@@ -9,14 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("폰트 적용")
-                .font(.custom("Pretendard-reguler", size:40))
+        ZStack{
+            VStack {
+                Spacer()
+                HStack{
+                    Image(systemName: "location.fill")
+                        .foregroundColor(.cwGray4)
+                        .imageScale(.small)
+                        .foregroundStyle(.tint)
+        
+                    Text("포항시 지곡동")
+                        .font(.custom("Pretendard-reguler", size:16))
+                        .foregroundColor(.cwGray4)
+                }.padding(.bottom, 40)
+                
+                Text("오늘 세차하기 좋아요")
+                    .font(.custom("Pretendard-bold", size:24))
+                    .foregroundColor(.cwGray5)
+                    .padding(.bottom, 8)
+                Text("10일간 비 올 확률이 낮아요")
+                    .font(.custom("Pretendard-reguler", size:16))
+                    .foregroundColor(.cwGray4)
+                Spacer()
+            }
+           
         }
-        .padding()
     }
 }
 
