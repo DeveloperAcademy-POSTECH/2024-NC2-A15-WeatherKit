19644b3a51a61284084432eae294c5c1d379ba75
다은0 <jenny8612@naver.com>
2024-06-18 15:16:39 +0900

    메인뷰 레이아웃 완성

--- CarWashDay/ContentView.swift
+++ CarWashDay/ContentView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     var body: some View {
+            
             VStack {
                 
                 Spacer()
@@ -17,38 +20,103 @@ struct ContentView: View {
                     Image(systemName: "location.fill")
                         .foregroundColor(.cwGray4)
                         .imageScale(.small)
-                        .foregroundStyle(.tint)
                     
                     Text("포항시 지곡동")
-                        .font(.custom("Pretendard-reguler", size:16))
+                        .font(.custom("Pretendard-Reguler", size:16))
                         .foregroundColor(.cwGray4)
                 }
                 .padding(.bottom, 40)
                 
                 Text("오늘 세차하기 좋아요")
-                    .font(.custom("Pretendard-bold", size:24))
+                    .font(.custom("Pretendard-Bold", size:24))
                     .foregroundColor(.cwGray5)
                     .padding(.bottom, 8)
                 
                 Text("10일간 비 올 확률이 낮아요")
-                    .font(.custom("Pretendard-reguler", size:16))
+                    .font(.custom("Pretendard-Reguler", size:16))
                     .foregroundColor(.cwGray4)
                     .padding(.bottom, 42)
                 
                 Image("SunCar")
                 
-                ScrollView{
-                    RoundedRectangle(cornerRadius: 16)
-                        .foregroundColor(.cwBlueList)
+                ScrollView(.vertical, showsIndicators: false){
+                    VStack{
+                        Spacer()
+                            .padding(.top, 10)
+                        HStack{
+                            Text("10일간의 일기 예보")
+                                .font(.custom("Pretendard-Regular", size:14))
+                                .foregroundColor(.cwGray2)
+                            
+                            Spacer()
+                        }
+                        .padding(.bottom, 9)
+                        
+                        Divider()
+                            .padding(.bottom, 9)
+                        
+                        ForEach(1..<11){number in
+                            HStack{
+                                GeometryReader { geometry in
+                                    let totalWidth = geometry.size.width
+                                    let columnWidth1 = totalWidth * 0.25
+                                    let columnWidth2 = totalWidth * 0.5
+                                    let columnWidth3 = totalWidth * 0.25
+                                    
+                                    let columns = [
+                                        GridItem(.fixed(columnWidth1), spacing: 0, alignment: .center),
+                                        GridItem(.fixed(columnWidth2), spacing: 0, alignment: .center),
+                                        GridItem(.fixed(columnWidth3), spacing: 0, alignment: .center)
+                                    ]
+                                    LazyVGrid(columns: columns){
+                                        HStack{
+                                            Text("오늘")
+                                                .font(.custom("Pretendard-Medium", size:20))
+                                                .foregroundColor(.cwGray4)
+                                            Spacer()
+                                        }
+                                        HStack{
+                                            Image(systemName: "sun.max.fill")
+                                                .foregroundColor(.cwYellowSun)
+                                                .font(.system(size: 20))
+                                            Text("맑음")
+                                                .font(.custom("Pretendard-Regular", size:16))
+                                                .foregroundColor(.cwGray3)
+                                                .padding(.leading, 16)
+                                            Spacer()
+                                        }
+                                        HStack{
+                                            Spacer()
+                                            Text("0%")
+                                                .font(.custom("Pretendard-Medium", size:20))
+                                                .foregroundColor(.cwGray3)
+                                        }
+                                        
+                                    }
+                                }
+                                .padding(.bottom, 32)
+                            }
+                        }
+                        
+                    }
+                    .padding(.horizontal, 24)
+                    .background(Color(.cwBlueList))
+                    .cornerRadius(16)
+                    Rectangle()
+                        .foregroundColor(.cwBlueBg)
+                        .frame(height: 64)
                 }
+                .edgesIgnoringSafeArea(.all)
                 
-                Spacer()
+                
+                
             }
             .padding(.horizontal, 20)
             .background(Color(.cwBlueBg))
-       
+            
+        }
     }
-}
+
 
 #Preview {
     ContentView()
